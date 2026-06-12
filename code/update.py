78ef0fee3611103f41a588a48e562acd9e348859
Cody Baker <51133164+CodyCBakerPhD@users.noreply.github.com>
2026-06-11 22:56:22 -0400

    Update update.py

--- code/update.py
+++ code/update.py
@@ -169,4 +169,4 @@ def _run(base_directory: pathlib.Path, limit: int | None) -> None:
 if __name__ == "__main__":
     repo_head = pathlib.Path(__file__).parent.parent
 
-    _run(repo_head, limit=5_000)
+    _run(repo_head, limit=3_000)
